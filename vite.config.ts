3e9458a6068c04f156c9f775c600ce763cbb2ff8
nhanquach <qtrongnhan@gmail.com>
2024-12-30 18:42:01 +0700

    feat: init vue app as chrome extension

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [vue(),
+  viteStaticCopy({
+    targets: [
+      {
+        src: 'public/manifest.json',
+        dest: '.',
+      }
+    ],
+  }),],
+  build: {
+    outDir: 'build',
+    rollupOptions: {
+      input: {
+        main: './index.html',
+      },
+    },
+  },
+})
