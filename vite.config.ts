7e1c3dd5f6ca1a81c924378be1c0b847819d8132
nhanquach <qtrongnhan@gmail.com>
2025-01-09 10:45:20 +0700

    feat: support eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,26 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import { viteStaticCopy } from 'vite-plugin-static-copy';
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: 'public/manifest.json',
-        dest: '.',
-      }
-    ],
-  }),],
+  plugins: [
+    vue(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "public/manifest.json",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
   build: {
-    outDir: 'build',
+    outDir: "build",
     rollupOptions: {
       input: {
-        main: './index.html',
+        main: "./index.html",
       },
     },
   },
-})
+});
